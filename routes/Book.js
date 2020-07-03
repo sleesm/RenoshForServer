080ebca704891a2718426d0162e5f80b07c346eb
SeongMinKwon <sleepingsm813@gmail.com>
2020-07-03 13:57:05 +0900

    Book API GET

--- routes/Book.js
+++ routes/Book.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/", (req, res)=>{
+    res.send("list of books");
+});
+
+router.get("/:bookid", (req, res)=>{
+    res.send(`a book. id: ${req.params.bookid}`);
+});
 module.exports = router;
