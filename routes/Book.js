497c4cff70627fb37f88741cf5cdc6d105b9b482
SeongMinKwon <sleepingsm813@gmail.com>
2020-07-07 22:13:24 +0900

    Update Book API GET, GET with ID, POST and add DELETE

--- routes/Book.js
+++ routes/Book.js
@@ -1,26 +1,38 @@
+
 const express = require('express');
 const router = express.Router();
-const { CosmosClient } = require("@azure/cosmos");
+const { CosmosClient, TriggerType } = require("@azure/cosmos");
 const endpoint = "https://renosh.documents.azure.com:443/"; // Add your endpoint
-const key = "VGGuRGqhhxhHNuDbdpV0ypJo3DlGRtchkGH1ofFgaq3XFX4ynqcJlWbVVoRMk48Gn8jQjxHjfFKpcvbTarRd4A=="; // Add the masterkey of the endpoint   key
+const key = "key"; // Add the masterkey of the endpoint   key
 const client = new CosmosClient({ endpoint, key });
 
-let count = 0;
-let bookid =[];
+const database = client.database('renosh');
+const container = database.container('book' );
+
+async function getListOfBooks(req, res){
+    const { resources: bookList } = await container.items.readAll().fetchAll();
+    console.log(bookList)
+    // res.json(bookList); // error 발생 Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+}
 
 router.get("/", (req, res)=>{
     res.send("list of books");
-});
+    getListOfBooks(req, res);
+})
+
+async function getBookWithId(req, res){
+    const bookid = req.params.bookid;
+    const { resource: book } = await container.item(bookid, undefined).read();
+    console.log(book);
+    // res.json(book); // 동일 error 발생
+}
 
 router.get("/:bookid", (req, res)=>{
     res.send(`a book. id: ${req.params.bookid}`);
-});
+    getBookWithId(req, res);
+})
 
 async function postBookInfo(req, res){
-    const { database } = await client.databases.createIfNotExists({ id: 'renosh' });
-    const { container } = await database.containers.createIfNotExists({ id: 'book' });
-    const { resources: itemDefList } = await container.items.readAll().fetchAll();
-
     const bookinfo = [
         {
             // id : "1",
@@ -40,19 +52,48 @@ async function postBookInfo(req, res){
 
     for(const book of bookinfo){
         const { resource } = await container.items.create(book);
-        res.send(resource.id);
+        console.log(resource.id);
     }
+
+    // res.json(resource.id); // 동일 error 발생
+    res.send("Book Info added successfully.")
 }
 
 router.post("/", (req, res)=>{
     postBookInfo(req, res);
+})
+
+// async function putBookInfo(req, res){
+
+//     const bookinfo =
+//     {
+//         // id : "1",
+//         bookName: "The Little Prince",
+//         author: "Antoine de Saint-Exupéry",
+//         image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
+//         epubURL: "https://pdfstop.com/get-download?file=838"
+//     }
+//     const { resource } = await container.items.create(book);
+//     console.log(resource.id);
+// }
+
+// router.put("/", (req, res)=>{
+//     res.send("Book info updated successfully.");
+// })
+
+async function deleteBook(req, res){
+    const bookid = req.params.bookid;
+    try{
+        const {resource: item} = await container.item(bookid, undefined).delete();
+        res.status(200).json({"book id":bookid});
+        console.log("Book deleted successfully");
+    } catch(error){
+        res.status(500).send(error);
+    }
+}
+
+router.delete("/:bookid",(req, res)=>{
+    deleteBook(req, res);
 });
-
-
-router.put("/", (req, res)=>{
-    res.send("Book info updated successfully.");
-});
-
-
 
 module.exports = router;
