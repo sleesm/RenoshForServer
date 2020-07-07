fc605da4095fb8d8415161fee27a9d7885e006d8
SeongMinKwon <sleepingsm813@gmail.com>
2020-07-07 23:14:42 +0900

    add try-catch to  GET, GET with ID, and POST

--- routes/Book.js
+++ routes/Book.js
@@ -10,53 +10,53 @@ const database = client.database('renosh');
 const container = database.container('book' );
 
 async function getListOfBooks(req, res){
-    const { resources: bookList } = await container.items.readAll().fetchAll();
-    console.log(bookList)
-    // res.json(bookList); // error 발생 Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    try{
+        const { resources: bookList } = await container.items.readAll().fetchAll();
+        console.log(bookList)
+        res.json(bookList); //error try-catch 사용
+    }catch(error){
+        res.status(500).send(error);
+    }
 }
 
 router.get("/", (req, res)=>{
-    res.send("list of books");
+    // res.send("list of books");
     getListOfBooks(req, res);
 })
 
 async function getBookWithId(req, res){
-    const bookid = req.params.bookid;
-    const { resource: book } = await container.item(bookid, undefined).read();
-    console.log(book);
-    // res.json(book); // 동일 error 발생
+    try{
+        const bookid = req.params.bookid;
+        const { resource: book } = await container.item(bookid, undefined).read();
+        console.log(book);
+        res.json(book); // 동일 error 발생
+    }catch(error){
+        res.status(500).send(error);
+    }
 }
 
 router.get("/:bookid", (req, res)=>{
-    res.send(`a book. id: ${req.params.bookid}`);
+    // res.send(`a book. id: ${req.params.bookid}`);
     getBookWithId(req, res);
 })
 
 async function postBookInfo(req, res){
-    const bookinfo = [
-        {
-            // id : "1",
-            bookName: "The Little Prince",
-            author: "Antoine de Saint-Exupéry",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
-            epubURL: "https://pdfstop.com/get-download?file=838"
-        },
-        {
-            // id : "2",
-            bookName: "The Little Prince2",
-            author: "Antoine de Saint-Exupéry",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
-            epubURL: "https://pdfstop.com/get-download?file=838"
-        }
-    ];
-
-    for(const book of bookinfo){
-        const { resource } = await container.items.create(book);
-        console.log(resource.id);
+    const bookinfo =
+    {
+        // id : "1",
+        bookName: "The Little Prince",
+        author: "Antoine de Saint-Exupéry",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
+        epubURL: "https://pdfstop.com/get-download?file=838"
     }
 
-    // res.json(resource.id); // 동일 error 발생
-    res.send("Book Info added successfully.")
+    try{
+        const { resource } = await container.items.create(bookinfo);
+        console.log(resource.id);
+        res.json(resource.id);
+    }catch(error){
+        res.status(500).send(error);
+    }
 }
 
 router.post("/", (req, res)=>{
