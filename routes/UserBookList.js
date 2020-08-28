037c60d85fddc18d21dc5e79081f4643ad05ce0d
SeongMinKwon <sleepingsm813@gmail.com>
2020-08-28 13:28:01 +0900

    create UserBookList GET, POST, PUT API

--- routes/UserBookList.js
+++ routes/UserBookList.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const { getMyBookListOfUsersById,updateMyBookListById} = require('./UserBookList_cosmos');
+const { getUserBookListByUserId, getMyBookListOfUsersByUserId,postMyBookListOfUsersById, updateMyBookListById} = require('./UserBookList_cosmos');
 const router = express.Router();
+
+// get a specfic user's userbooklist filed by user id
+router.get('/:userid', (req, res) => {
+    getUserBookListByUserId(req, res);
+});
 
 // get a specfic user's my book list by user id
 router.get('/:userid/mybooklist', (req, res) => {
-    getMyBookListOfUsersById(req, res);
+    getMyBookListOfUsersByUserId(req, res);
+});
+
+// create my book list when first 'startReading' clicked
+router.post('/:userid', (req, res) => {
+    postMyBookListOfUsersById(req, res);
 });
 
 // put(update) a specific user's my book list by id
-router.put('/:userid/mybooklist', (req, res) => {
+router.put('/:userid/:userbooklistid/mybooklist', (req, res) => {
     updateMyBookListById(req, res);
 });
 
